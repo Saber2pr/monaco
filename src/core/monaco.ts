b4c96e17730a4b543b64b0ee695d99c4464226ba
muhai <muhai.li@tuya.com>
2022-03-28 15:38:44 +0800

    chore: update

--- src/core/monaco.ts
+++ src/core/monaco.ts
@@ -2,9 +2,9 @@
  * @Author: saber2pr
  * @Date: 2020-05-05 20:55:00
  * @Last Modified by: saber2pr
- * @Last Modified time: 2020-05-05 21:14:08
+ * @Last Modified time: 2022-03-28 15:38:29
  */
-import * as Monaco from 'monaco-editor/esm/vs/editor/editor.main.js'
+import * as Monaco from 'monaco-editor/min/vs/editor/editor.main.js'
 
 export type IMonaco = typeof import('monaco-editor')
 
