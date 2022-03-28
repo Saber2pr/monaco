b832c4b39c3d3e61d1e579755c689b92481b5b85
muhai <muhai.li@tuya.com>
2022-03-28 15:31:13 +0800

    chore: update

---
+++ src/react/diff-editor.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from 'react'
+
+import { createDiffEditor, DiffEditorAPI } from '../core'
+
+export interface DiffEditorProps {
+  original: string
+  modified: string
+  language?: string
+  onInit?: (editor: DiffEditorAPI) => any
+  deps?: any[]
+}
+
+export const DiffEditor: React.FC<DiffEditorProps> = ({
+  onInit,
+  original,
+  modified,
+  language,
+  deps,
+}) => {
+  const ref = useRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (ref.current) {
+      const editor = createDiffEditor(ref.current, original, modified, language)
+      if (onInit) {
+        onInit(editor)
+      }
+    }
+  }, deps)
+
+  return <div ref={ref}></div>
+}
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,1 +1,2 @@
+export * from './diff-editor';
 export * from './editor';
