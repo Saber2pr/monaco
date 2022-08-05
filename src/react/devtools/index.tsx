1af0a80d23ee293839dc6105bfcec490cdfdbd6a
muhai <muhai.li@tuya.com>
2022-08-05 09:37:51 +0800

    feat: devtools

---
+++ src/react/devtools/index.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState, useCallback } from 'react'
+import * as reactDevtools from 'react-devtools-inline/frontend'
+import { Container } from './index.style'
+
+const timeout = (delay = 1000) =>
+  new Promise((resolve) => setTimeout(resolve, delay))
+
+export type StatusType = 'info' | 'warning' | 'error' | 'success' | 'clear'
+
+export type DevToolProps = {
+  hidden?: boolean
+  sandboxId?: string
+  browserTheme?: 'light' | 'dark'
+}
+
+export const DevTools: React.FC<DevToolProps> = (props) => {
+  const [ReactDevTools, setDevTools] = useState(null)
+  const unmounted = React.useRef(false)
+
+  const loadIframe = useCallback(async () => {
+    let iframe = document.getElementById(props.sandboxId) as HTMLIFrameElement
+
+    // iframe hasn't initialized or just isn't there
+    while (iframe === null && !unmounted.current) {
+      // Retry every second
+      // eslint-disable-next-line
+      await timeout(1000)
+      iframe = document.getElementById(props.sandboxId) as HTMLIFrameElement
+    }
+
+    if (iframe) {
+      const { contentWindow } = iframe
+
+      window.addEventListener('message', (event) => {
+        const message = event.data
+        if (message.type === 'activate-react-devtools') {
+          setDevTools(reactDevtools.initialize(contentWindow))
+        }
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    loadIframe()
+    return () => {
+      unmounted.current = true
+    }
+  }, [loadIframe])
+
+  if (props.hidden) {
+    return null
+  }
+
+  return (
+    <Container>
+      {ReactDevTools ? (
+        <ReactDevTools browserTheme={props.browserTheme || 'light'} />
+      ): <span style={{color: '#000'}}>[Waiting for Sandbox]...</span>}
+    </Container>
+  )
+}
---
+++ src/react/devtools/initLatestDevTools.ts
@@ -0,0 +1,27 @@
+import {
+  initialize as initializeDevTools,
+  activate,
+} from 'react-devtools-inline/backend'
+
+async function initializeReactDevToolsLegacy() {
+  if (!window.opener) {
+    // The dispatch needs to happen before initializing, so that the backend can already listen
+    window.parent.postMessage({ type: 'activate-react-devtools' }, '*')
+
+    // @ts-ignore
+    if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== 'undefined') {
+      try {
+        // @ts-ignore We need to make sure that the existing chrome extension doesn't interfere
+        delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__
+      } catch (e) {
+        /* ignore */
+      }
+    }
+    // Call this before importing React (or any other packages that might import React).
+    initializeDevTools(window)
+    activate(window)
+  }
+}
+
+// @ts-ignore
+window.initializeReactDevToolsLegacy = initializeReactDevToolsLegacy
