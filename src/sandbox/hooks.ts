3b18ce27009f8c4b9ff4163c9f9f923137f9b5a8
saber2pr <saber2pr@gmail.com>
2022-09-02 16:35:25 +0800

    fix: parent

--- src/sandbox/hooks.ts
+++ src/sandbox/hooks.ts
@@ -13,9 +13,9 @@ export const ConsoleHook = `<script data-type="${KEYS.__SANDBOX_HOOK__}">
 	console.log = function() {
 		origin_log.apply(this, arguments)
 		var output = Array.from(arguments).join(" ")
-		top.postMessage({method: "${KEYS.__MESSAGE_CONSOLE__}", value: output}, top.location.origin)
+		parent.postMessage({method: "${KEYS.__MESSAGE_CONSOLE__}", value: output}, parent.location.origin)
 	}
 	window.addEventListener('error', event => {
-		top.postMessage({method: "${KEYS.__MESSAGE_CONSOLE_ERROR__}", value: event.message}, top.location.origin)
+		parent.postMessage({method: "${KEYS.__MESSAGE_CONSOLE_ERROR__}", value: event.message}, parent.location.origin)
 	})
 })()</script>`
