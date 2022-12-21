cc34637518fa250552487ba5de714faed999484e
saber2pr <saber2pr@gmail.com>
2022-12-21 13:47:29 +0800

    chore: update

--- src/react/devtools/bridge/config.ts
+++ src/react/devtools/bridge/config.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_UID_FRONTEND = 'react-devtools.frontend'
 export const DEFAULT_UID_BACKEND = 'react-devtools.backend'
-export const DEFAULT_HOST = 'localhost'
+export const DEFAULT_HOST = '127.0.0.1'
 export const DEFAULT_PORT = 60030
-export const DEFAULT_SOCKETURL = `http://${DEFAULT_HOST}:${DEFAULT_PORT}`
+export const DEFAULT_SOCKETURL = `ws://${DEFAULT_HOST}:${DEFAULT_PORT}`
