f51d9ce11f8db3eefe855c24114ab2d8a47559c4
saber2pr <saber2pr@gmail.com>
2022-12-21 11:59:00 +0800

    chore: update

--- src/react/devtools/install-sandbox-wall/initializeReactDevToolsLegacy.ts
+++ src/react/devtools/install-sandbox-wall/initializeReactDevToolsLegacy.ts
@@ -8,29 +8,27 @@ import { createSocketBridgeWall } from '../bridge/client'
 
 // inject to app
 export async function initializeReactDevToolsLegacyWall() {
-  if (!window.opener) {
-    // create socket connection
-    const wall = await createSocketBridgeWall({ UID: DEFAULT_UID_BACKEND })
-    const bridge = createBridge(global, wall)
+  // create socket connection
+  const wall = await createSocketBridgeWall({ UID: DEFAULT_UID_BACKEND })
+  const bridge = createBridge(global, wall)
 
-    // The dispatch needs to happen before initializing, so that the backend can already listen
-    wall.send('activate-react-devtools', null)
+  // The dispatch needs to happen before initializing, so that the backend can already listen
+  wall.send('activate-react-devtools', null)
 
-    // @ts-ignore
-    if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== 'undefined') {
-      try {
-        // @ts-ignore We need to make sure that the existing chrome extension doesn't interfere
-        delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__
-      } catch (e) {
-        /* ignore */
-      }
+  // @ts-ignore
+  if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== 'undefined') {
+    try {
+      // @ts-ignore We need to make sure that the existing chrome extension doesn't interfere
+      delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__
+    } catch (e) {
+      /* ignore */
     }
-    // Call this before importing React (or any other packages that might import React).
-    initializeDevTools(window)
-    activate(window, {
-      bridge,
-    })
   }
+  // Call this before importing React (or any other packages that might import React).
+  initializeDevTools(window)
+  activate(window, {
+    bridge,
+  })
 }
 
 export default initializeReactDevToolsLegacyWall
