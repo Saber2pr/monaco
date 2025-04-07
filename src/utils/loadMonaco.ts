0e51ded0b1b72b0d16e28c3099dba4387c68a2d4
muhai <muhai.li@tuya.com>
2025-04-07 11:48:32 +0800

    chore: update

--- src/utils/loadMonaco.ts
+++ src/utils/loadMonaco.ts
@@ -1,9 +1,11 @@
 import loader from '@monaco-editor/loader'
 
 export const loadMonaco = async (
-  params: Parameters<typeof loader.config>[0]
+  params?: Parameters<typeof loader.config>[0]
 ) => {
-  loader.config(params)
+  if (params) {
+    loader.config(params)
+  }
   const moanco = await loader.init()
   return moanco
 }
